977bf53610c3fe5e7f359af5ad3f81e8d908d163
Cristiano <er.cristiano.gotardi@slbenfica.pt>
2025-01-20 21:41:09 +0000

    test arithmetic functions

--- examples/basic-math/src/arithmetic.test.js
+++ examples/basic-math/src/arithmetic.test.js
@@ -1,9 +1,26 @@
 import { describe, it, expect } from 'vitest';
+import { add, subtract, multiply, divide } from './arithmetic.js'
 
-describe.todo('add', () => {});
+describe('add', () => {
+    it('should add two positive numbers', () => {
+        expect(add(2, 2)).toBe(4)
+    })
+});
 
-describe.todo('subtract', () => {});
+describe('subtract', () => {
+    it('should subtract two positive numbers', () => {
+        expect(subtract(2, 2)).toBe(0)
+    })
+});
 
-describe.todo('multiply', () => {});
+describe('multiply', () => {
+    it('should multiply two positive numbers', () => {
+        expect(multiply(2, 2)).toBe(4)
+    })
+});
 
-describe.todo('divide', () => {});
+describe('divide', () => {
+    it('should divide two positive numbers', () => {
+        expect(divide(2,2)).toBe(1)
+    })
+});
