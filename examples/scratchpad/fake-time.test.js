32d731770b1f24dfead123c857a5a634ef2131f6
Cristiano Gotardi <crgotardi@gmail.com>
2025-01-28 22:01:01 +0000

    mocking time

--- examples/scratchpad/fake-time.test.js
+++ examples/scratchpad/fake-time.test.js
@@ -1,13 +1,29 @@
-import { vi, describe, it, expect } from 'vitest';
+import { vi, describe, it, expect, afterEach, beforeEach } from 'vitest';
 
 vi.useFakeTimers();
 
 function delay(callback) {
   setTimeout(() => {
     callback('Delayed');
-  }, 1000);
+  }, 50000);
 }
 
 describe('delay function', () => {
-  it.todo('should call callback after delay', () => {});
+  beforeEach(() => {
+    vi.useFakeTimers()
+    vi.setSystemTime('2024-02-29')
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
+  })
+
+  it('should call callback after delay', () => {
+    const callback = vi.fn()
+
+    delay(callback)
+    vi.advanceTimersToNextTimer()
+
+    expect(callback).toHaveBeenCalled()
+  });
 });
