06ffe45bf9b17ffb3d1eba46987dd246b8e8ae73
Cristiano Gotardi <crgotardi@gmail.com>
2025-01-28 21:07:42 +0000

    spy and mock

--- examples/element-factory/src/alert-button.test.jsx
+++ examples/element-factory/src/alert-button.test.jsx
@@ -1,14 +1,28 @@
-import { render, screen, act } from '@testing-library/react';
+import { render, screen, act, vi } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import { AlertButton } from './alert-button';
 
-describe.todo('AlertButton', () => {
+describe('AlertButton', () => {
   beforeEach(() => {});
 
   afterEach(() => {});
 
   it('should render an alert button', async () => {});
 
-  it('should trigger an alert', async () => {});
+  it('should trigger an alert', async () => {
+    render(<AlertButton />)
+    const alertSpy = vi.spyOn(window, 'alert').mockImplementation(() => 'Hello')
+
+    const input = screen.getByLabelText('Message')
+    const button = screen.getByRole('button', { name: /trigger alert/i })
+
+    await act(async () => {
+      await userEvent.clear(input)
+      await userEvent.type(input, 'Hello')
+      await userEvent.click(button)
+    })
+
+    expect(alertSpy).toHaveBeenCalledWith('Hello')
+  });
 });
--- examples/scratchpad/index.test.js
+++ examples/scratchpad/index.test.js
@@ -1,5 +1,36 @@
-import { test, expect } from 'vitest';
+import { describe, it, expect, vi } from 'vitest';
 
-test('a super simple test', () => {
-  expect(true).toBe(true);
-});
+const logSpy = vi.spyOn(console, 'log')
+const randomSpy = vi.spyOn(Math, 'random').mockImplementation(() => 0.5)
+
+describe('Spy', () => {
+  it('should log hello world', () => {
+    console.log('hello world')
+
+    expect(logSpy).toHaveBeenCalled()
+    expect(logSpy).toHaveBeenCalledOnce()
+    expect(logSpy).toHaveBeenCalledTimes(1)
+    expect(logSpy).toHaveBeenCalledWith('hello world')
+  })
+})
+
+describe('Mock', () => {
+  it('should mock log of hello world', () => {
+    const mockFn = vi.fn()
+    mockFn('hello world (mocked)')
+  
+    expect(mockFn).toHaveBeenCalled()
+    expect(mockFn).toHaveBeenCalledOnce()
+    expect(mockFn).toHaveBeenCalledTimes(1)
+    expect(mockFn).toHaveBeenCalledWith('hello world (mocked)')
+  })
+})
+
+describe('Spy with Mock', () => {
+  it('should return a random number between 0 and 1', () => {
+    const result = Math.random()
+
+    expect(randomSpy).toHaveBeenCalled()
+    expect(result).toBe(0.5)
+  })
+})
